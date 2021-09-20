c128fbc145528fbd7755ca1b0b7c4d89dc290230
theabhishekg98 <theabhishekg98@gmail.com>
2021-09-19 17:52:14 -0700

    Set up React with backend SQL server

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,42 @@
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
+
 
 function App() {
 
-  const []
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  return (
+  useEffect(() => {
+    axios.get('http://localhost:8000/getCredentials')
+    .then((response) => {
+      console.log(response);
+    });
+  }, []);
+
+  const submitCredentials = () => {
+    axios.post('http://localhost:8000/addCredentials', {
+      email: email,
+      password: password
+    }).then(() => {
+      alert("Added user credentials successfully!");
+    });
+  };
+
+  return ( 
     <div className="App">
       <h1>Login</h1>
       <div className="loginForm">
         <label>Email:</label>
-        <input type="email" name="email"></input>
+        <input type="email" name="email" onChange={(e) => {
+          setEmail(e.target.value)
+        }}></input>
         <label>Password:</label>
-        <input type="password" name="password"></input>
-        <button name="submit">Enter</button> 
+        <input type="password" name="password"onChange={(e) => {
+          setPassword(e.target.value)
+        }}></input>
+        <button name="submit" onClick={submitCredentials}>Enter</button> 
       </div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const mysql = require('mysql');
+const cors = require('cors');
+// const bodyParser = require('body-parser');
+
 
 const db = mysql.createPool({
     host: 'localhost',
@@ -9,10 +12,28 @@ const db = mysql.createPool({
     database: 'UberEatsDatabase'
 });
 
-app.get("/", (req, res) => {
-    
-})
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
-app.listen(3001, () => {
-    console.log("App listening on port 3001");
+app.get('/getCredentials', (req, res) => {
+    const sqlSelect = "SELECT * from login";
+    db.query(sqlSelect, (err, result)=> {
+        res.send(result);
+    }); 
+});
+
+app.post('/addCredentials', (req, res) => {
+    email = req.body.email;
+    password = req.body.password;
+    // console.log(email);
+    // console.log(password);
+    const sqlInsert = "INSERT INTO login VALUES (?,?)";
+    db.query(sqlInsert, [email, password], (err, result)=> {
+        console.log(result);
+    });
+});
+
+app.listen(8000, () => {
+    console.log("App listening on port 8000");
 });
