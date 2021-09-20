f9ec232df68fd699198c097b0365403cf3b62d6d
theabhishekg98 <theabhishekg98@gmail.com>
2021-09-19 20:03:31 -0700

    Added registration flow and login authentication

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,13 @@ import axios from 'axios';
 
 
 function App() {
+  const [emailReg, setEmailReg] = useState('');
+  const [passwordReg, setPasswordReg] = useState('');
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [emailLogin, setEmailLogin] = useState('');
+  const [passwordLogin, setPasswordLogin] = useState('');
+
+  const [loginStatus, setLoginStatus] = useState('');
 
   useEffect(() => {
     axios.get('http://localhost:8000/getCredentials')
@@ -15,12 +19,28 @@ function App() {
     });
   }, []);
 
-  const submitCredentials = () => {
-    axios.post('http://localhost:8000/addCredentials', {
-      email: email,
-      password: password
-    }).then(() => {
-      alert("Added user credentials successfully!");
+  const Register = () => {
+    axios.post('http://localhost:8000/register', {
+      email: emailReg,
+      password: passwordReg
+    }).then((response) => {
+      // alert("Added user credentials successfully!");
+      console.log(response);
+    });
+  };
+
+  const Login = () => {
+    axios.post('http://localhost:8000/login', {
+      email: emailLogin,
+      password: passwordLogin
+    }).then((response) => {
+      // alert("User credentials authenticated successfully!");
+      if(response.data.message) {
+        setLoginStatus(response.data.message);
+      } else {
+        setLoginStatus("Login Successful!");
+      }
+      console.log(response);
     });
   };
 
@@ -30,14 +50,29 @@ function App() {
       <div className="loginForm">
         <label>Email:</label>
         <input type="email" name="email" onChange={(e) => {
-          setEmail(e.target.value)
+          setEmailLogin(e.target.value)
         }}></input>
         <label>Password:</label>
-        <input type="password" name="password"onChange={(e) => {
-          setPassword(e.target.value)
+        <input type="password" name="password" onChange={(e) => {
+          setPasswordLogin(e.target.value)
         }}></input>
-        <button name="submit" onClick={submitCredentials}>Enter</button> 
+        <button name="submit" onClick={Login}>Login</button> 
       </div>
+       
+      <h1>Register</h1>
+      <div className="loginForm">
+        <label>Email:</label>
+        <input type="email" name="email" onChange={(e) => {
+          setEmailReg(e.target.value)
+        }}></input>
+        <label>Password:</label>
+        <input type="password" name="password" onChange={(e) => {
+          setPasswordReg(e.target.value)
+        }}></input>
+        <button name="submit" onClick={Register}>Register</button> 
+      </div>
+
+      <h1>{loginStatus}</h1>
     </div>
   );
 }
