866e36b88c63dc109512439546621e4b8ef7eccb
theabhishekg98 <theabhishekg98@gmail.com>
2021-09-21 03:05:16 -0700

    Login Registration routes Logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,80 +1,31 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import './App.css';
-import axios from 'axios';
+// import axios from 'axios';
+import { Route } from 'react-router-dom';
+import Home from './Home';
+import CustomerLogin from './CustomerLogin';
+import CustomerRegister from './CustomerRegister';
+import RestaurantLogin from './RestaurantLogin';
+import RestaurantRegister from './RestaurantRegister';
+import Landing from './Landing';
 
+// import Landing from './Landing';
 
 function App() {
-  const [emailReg, setEmailReg] = useState('');
-  const [passwordReg, setPasswordReg] = useState('');
-
-  const [emailLogin, setEmailLogin] = useState('');
-  const [passwordLogin, setPasswordLogin] = useState('');
-
-  const [loginStatus, setLoginStatus] = useState('');
-
-  useEffect(() => {
-    axios.get('http://localhost:8000/getCredentials')
-    .then((response) => {
-      console.log(response);
-    });
-  }, []);
-
-  const Register = () => {
-    axios.post('http://localhost:8000/register', {
-      email: emailReg,
-      password: passwordReg
-    }).then((response) => {
-      // alert("Added user credentials successfully!");
-      console.log(response);
-    });
-  };
-
-  const Login = () => {
-    axios.post('http://localhost:8000/login', {
-      email: emailLogin,
-      password: passwordLogin
-    }).then((response) => {
-      // alert("User credentials authenticated successfully!");
-      if(response.data.message) {
-        setLoginStatus(response.data.message);
-      } else {
-        setLoginStatus("Login Successful!");
-      }
-      console.log(response);
-    });
-  };
-
-  return ( 
-    <div className="App">
-      <h1>Login</h1>
-      <div className="loginForm">
-        <label>Email:</label>
-        <input type="email" name="email" onChange={(e) => {
-          setEmailLogin(e.target.value)
-        }}></input>
-        <label>Password:</label>
-        <input type="password" name="password" onChange={(e) => {
-          setPasswordLogin(e.target.value)
-        }}></input>
-        <button name="submit" onClick={Login}>Login</button> 
-      </div>
-       
-      <h1>Register</h1>
-      <div className="loginForm">
-        <label>Email:</label>
-        <input type="email" name="email" onChange={(e) => {
-          setEmailReg(e.target.value)
-        }}></input>
-        <label>Password:</label>
-        <input type="password" name="password" onChange={(e) => {
-          setPasswordReg(e.target.value)
-        }}></input>
-        <button name="submit" onClick={Register}>Register</button> 
-      </div>
-
-      <h1>{loginStatus}</h1>
+  return (
+    <div>
+      <Route exact path="/" component={Landing}></Route>
+      <Route exact path="/CustomerLogin" component={CustomerLogin}></Route>
+      <Route exact path="/CustomerRegister" component={CustomerRegister}></Route>
+      <Route exact path="/RestaurantLogin" component={RestaurantLogin}></Route>
+      <Route exact path="/RestaurantRegister" component={RestaurantRegister}></Route>
+      <Route exact path="/Home" component={Home}></Route>
     </div>
-  );
+    // <Route exact path="/" component={Home}></Route>
+    // <Route exact path="/" component={Home}></Route>
+    // <Route exact path="/" component={Home}></Route>
+    // <Route exact path="/" component={Home}></Route>
+  )
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ const cors = require('cors');
 const e = require("express");
 // const bodyParser = require('body-parser');
 
-
 const db = mysql.createPool({
     host: 'localhost',
     user: 'root',
@@ -18,32 +17,79 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.get('/getCredentials', (req, res) => {
-    const sqlSelect = "SELECT * from login";
+    const sqlSelect = "SELECT * from customer_login";
     db.query(sqlSelect, (err, result)=> {
         res.send(result);
     }); 
 });
 
-app.post('/register', (req, res) => {
+app.post('/customer_register', (req, res) => {
+    cname = req.body.name;
     email = req.body.email;
     password = req.body.password;
 
-    const sqlInsert = "INSERT INTO login VALUES (?,?)";
-    db.query(sqlInsert, [email, password], (err, result)=> {
+    console.log("1");
+
+    const sqlLoginCheck = "SELECT * from customer_login WHERE email = ?";
+    db.query(sqlLoginCheck, [email], (err, result)=> {
+        console.log("2");
         if(err){ 
+            console.log("3");
             res.send({err: err});
         } else {
-            console.log("Registration successful!");
-            console.log(result);
+            console.log("4");
+            if(result.length > 0) {
+                console.log("User already exists!");
+            } else {
+                console.log("5");
+                const sqlInsert = "INSERT INTO customer_login VALUES (?,?,?)";
+                db.query(sqlInsert, [cname, email, password], (err, result)=> {
+                    if(err){ 
+                        console.log("6");
+                        res.send({err: err});
+                    } else {
+                        console.log("Registration successful!");
+                        console.log(result);
+                    }
+                });
+            }
         }
     });
 });
 
-app.post('/login', (req, res) => {
+app.post('/restaurant_register', (req, res) => {
+    rname = req.body.name;
+    email = req.body.email;
+    password = req.body.password;
+    location = req.body.location;
+    const sqlLoginCheck = "SELECT * from restaurant_login WHERE email = ?";
+    db.query(sqlLoginCheck, [email], (err, result)=> {
+        if(err){ 
+            res.send({err: err});
+        } else {
+            if(result.length > 0) {
+                console.log("Restaurant already exists!");
+                res.send(result);
+            } else {
+                const sqlInsert = "INSERT INTO restaurant_login VALUES (?,?,?,?)";
+                db.query(sqlInsert, [rname, email, password, location], (err, result)=> {
+                    if(err){ 
+                        res.send({err: err});
+                    } else {
+                        console.log("Registration successful!");
+                        console.log(result);
+                    }
+                });
+            }
+        }
+    });
+});
+
+app.post('/customer_login', (req, res) => {
     email = req.body.email;
     password = req.body.password;
 
-    const sqlLoginCheck = "SELECT * from login WHERE email = ? AND password = ?";
+    const sqlLoginCheck = "SELECT * from customer_login WHERE email = ? AND password = ?";
     db.query(sqlLoginCheck, [email, password], (err, result)=> {
         // console.log("RESULT = ");
         // console.log(result);
@@ -61,6 +107,27 @@ app.post('/login', (req, res) => {
     });
 });
 
+app.post('/restaurant_login', (req, res) => {
+    email = req.body.email;
+    password = req.body.password;
+
+    const sqlLoginCheck = "SELECT * from restaurant_login WHERE email = ? AND password = ?";
+    db.query(sqlLoginCheck, [email, password], (err, result)=> {
+        // console.log("RESULT = ");
+        // console.log(result);
+        if(err){ 
+            res.send({err: err});
+        } else {
+            if(result.length > 0) {
+                console.log("Restaurant found!");
+                res.send(result);
+            } else {
+                console.log("Restaurant not found!");
+                res.send({message: "Incorrect username / password!"})
+            }
+        }
+    });
+});
 
 app.listen(8000, () => {
     console.log("App listening on port 8000");
