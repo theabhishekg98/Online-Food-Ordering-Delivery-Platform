8f932c9e26b9a18ab9cfbfe6f841a7c7096b31eb
theabhishekg98 <theabhishekg98@gmail.com>
2021-09-29 15:33:53 -0700

    Redirect page after login

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 // import axios from 'axios';
 import { Route } from 'react-router-dom';
-import Home from './Home';
+// import Home from './Home';
 import CustomerLogin from './CustomerLogin';
 import CustomerRegister from './CustomerRegister';
 import RestaurantLogin from './RestaurantLogin';
@@ -19,12 +19,12 @@ function App() {
       <Route exact path="/CustomerRegister" component={CustomerRegister}></Route>
       <Route exact path="/RestaurantLogin" component={RestaurantLogin}></Route>
       <Route exact path="/RestaurantRegister" component={RestaurantRegister}></Route>
-      <Route exact path="/Home" component={Home}></Route>
+      {/* <Route exact path="/Home" component={Home}></Route> */}
     </div>
-    // <Route exact path="/" component={Home}></Route>
-    // <Route exact path="/" component={Home}></Route>
-    // <Route exact path="/" component={Home}></Route>
-    // <Route exact path="/" component={Home}></Route>
+  // <Route exact path="/" component={Home}></Route>
+  // <Route exact path="/" component={Home}></Route>
+  // <Route exact path="/" component={Home}></Route>
+  // <Route exact path="/" component={Home}></Route>
   )
 }
 
---
+++ client/src/Home.js
@@ -0,0 +1,20 @@
+
+// import Nav from "./Nav";
+
+function Home() {
+    return (
+        <div className="home">
+            <h1>
+                UberEats Clone
+            </h1>
+            <Nav />
+            {/* Body */}
+
+            {/* {Sidebar} */}
+
+            {/* cards */}
+        </div>
+    )
+}
+
+export default Home;
