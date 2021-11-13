2ed00becbb5c11b9b992a4b2696cdb48e67a1d0a
Abhishek Ganesan <abhishek@Abhisheks-MacBook-Pro.local>
2021-11-13 11:48:53 -0800

    Routes for personalizing webapp like favorites etc

---
+++ new-backend/routes/Personlization.js
@@ -0,0 +1,34 @@
+const router = require("express").Router();
+const { checkAuth } = require("../utils/passport");
+var kafka = require('../kafka/client');
+
+router.post("/personalize/customer", checkAuth, async (req, res) => {
+
+    const request = {
+        query: req.query, params: req.params, body: req.body,
+    }
+  
+    kafka.make_request('ubereats.create.personalization',request, function(error,results){
+        if (error){
+            res.status(400).send(error)
+        }else{
+            res.status(200).send(results);
+        }
+    });
+});
+
+router.get("/personalize/customer/:id", checkAuth, async (req, res) => {
+
+    const request = {
+        query: req.query, params: req.params, body: req.body,
+    }
+
+    kafka.make_request('ubereats.get.personalization',request, function(error,results){
+        if (error){
+            res.status(400).send(error)
+        }else{
+            res.status(200).send(results);
+        }
+    });})
+
+module.exports = router;
