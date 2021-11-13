88d360b000b4cff7ec82c3b68c76b85eda066c70
Abhishek Ganesan <abhishek@Abhisheks-MacBook-Pro.local>
2021-11-13 10:55:50 -0800

    Login and registaration routes

---
+++ new-backend/routes/Register.js
@@ -0,0 +1,50 @@
+const router = require("express").Router();
+var kafka = require('../kafka/client');
+const jwt = require("jsonwebtoken");
+const secret = "CMPE273UBEREATS";
+
+router.post("/customer/register", async (req, res) => {
+
+    const request = {
+        query: req.query, params: req.params, body: req.body,
+    }
+   
+    kafka.make_request('ubereats.post.customer',request, async (error,results) => {
+        if (error){
+            res.status(400).send(error)
+        }else{
+            const payload = { CustomerId : results.CustomerId, EmailId: results.EmailId };
+            console.log(payload);
+            const token = await jwt.sign(payload, secret, {
+                    expiresIn: 1000000,
+            });
+            res.setHeader('token', "jwt " + token);
+            res.status(200).send(results);
+        }
+    });
+});
+
+router.post("/restaurant/register", (req, res) => {
+
+    const request = {
+        query: req.query, params: req.params, body: req.body,
+    }
+   
+    kafka.make_request('ubereats.post.restaurant',request, async (error,results)=>{
+        console.log("Here!!");
+        if (error){
+            res.status(400).send(error)
+        }else{
+            const payload = { RestaurantId : results.RestaurantId, EmailId: results.EmailId };
+            const token = await jwt.sign(payload, secret, {
+                    expiresIn: 1000000,
+            });
+            res.setHeader('token', "jwt " + token);
+            res.status(200).send(results);
+        }
+    });
+});
+
+
+
+module.exports = router;
