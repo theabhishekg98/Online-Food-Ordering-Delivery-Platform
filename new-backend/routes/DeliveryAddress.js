1925b769c4cc3579c0ab519944e5a3c698f69297
Abhishek Ganesan <abhishek@Abhisheks-MacBook-Pro.local>
2021-11-13 11:46:53 -0800

    Routes for delivery address after placing order

---
+++ new-backend/routes/DeliveryAddress.js
@@ -0,0 +1,37 @@
+
+const router = require("express").Router();
+var kafka = require('../kafka/client');
+const { checkAuth } = require("../utils/passport");
+
+
+router.post("/deliveryAddress/customer/:id", checkAuth, async (req, res)=> {
+   
+    const request = {
+        query: req.query, params: req.params, body: req.body,
+    }
+
+    kafka.make_request('ubereats.create.deliveryAddress',request, function(error,results){
+        if (error){
+            res.status(400).send(error)
+        }else{
+            res.status(200).send(results);
+        }
+    });
+});
+
+router.get("/deliveryAddress/customer/:id", checkAuth, async (req, res) => {
+    
+    const request = {
+        query: req.query, params: req.params, body: req.body,
+    }
+
+    kafka.make_request('ubereats.get.deliveryAddress',request, function(error,results){
+        if (error){
+            res.status(400).send(error)
+        }else{
+            res.status(200).send(results);
+        }
+    });
+});
+
+module.exports = router;
